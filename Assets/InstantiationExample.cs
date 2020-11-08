f3e3c3665893f55571372172f2a77e7206443e23
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-11-08 15:26:25 +0900

    add Assets and ProjectSettings

---
+++ Assets/InstantiationExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class InstantiationExample : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+    public Rigidbody projectile;
+    public float speed = 4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        // GameObject p = Instantiate(myPrefab, new Vector3(1.0f, 1.0f, 1.0f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody p = Instantiate(projectile, transform.position, transform.rotation);
+            Debug.Log(p.position[0]);
+            p.velocity = transform.forward * speed;
+        }
+    }
+}
