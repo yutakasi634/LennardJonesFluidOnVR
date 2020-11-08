d4d33a3978ea742a666b5a446202ce275fbe5d53
yutaka <p7dea87c7e4n7j@gmail.com>
2020-11-08 23:02:30 +0900

    rename InstantiationExample to InitialConfGenerator and Test to Particle

---
+++ Assets/InitialConfGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class InitialConfGenerator : MonoBehaviour
+{
+    public float temperature;
+    public float kb;
+    public float box_size;
+    public float mass;
+    public uint  total_particle_num;
+    public Rigidbody projectile;
+
+    private float sigma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        // GameObject p = Instantiate(myPrefab, new Vector3(1.0f, 1.0f, 1.0f), Quaternion.identity);
+       
+        temperature = 300.0f; 
+        //kb          = 0.8317f;          // mass:Da, length:Å, time:ps
+        kb          = 0.8317f * 10e-4f; // mass:Da, length:Å, time:0.01ps
+        box_size    = 5.0f;
+        total_particle_num = 500;
+        mass  = 1.0f;
+        sigma = Mathf.Sqrt(kb * temperature / mass);
+
+        // generate initial position
+        List<Vector3> coords_list = new List<Vector3>();
+        uint positioned_particle_num = 0;
+        uint trial_time              = 0;
+        while (positioned_particle_num < total_particle_num && trial_time < 500)
+        {
+            trial_time += 1;
+            Vector3 new_coord = new Vector3(Random.Range(-box_size, box_size),
+                                            Random.Range(-box_size, box_size),
+                                            Random.Range(-box_size, box_size));
+            bool acceptable = true;
+            if (coords_list.Count == 0) 
+            {
+                coords_list.Add(new_coord); 
+                positioned_particle_num += 1;
+                continue;
+            }
+
+            foreach (Vector3 fixed_pos in coords_list)
+            {
+                if ((fixed_pos - new_coord).magnitude < 1.0f) 
+                { 
+                    acceptable = false;
+                    break;
+                }
+            }
+            if (acceptable)
+            {
+                coords_list.Add(new_coord);
+                positioned_particle_num += 1;
+                //Debug.Log("determin particle " + positioned_particle_num.ToString() + "position");
+                trial_time = 0;
+            }
+        }
+
+        foreach (Vector3 coord_vec in coords_list)
+        {
+            Rigidbody particle = Instantiate(projectile, coord_vec, transform.rotation);
+            particle.velocity  = new Vector3(NormalizedRandom.Generate(0.0f, sigma),
+                                             NormalizedRandom.Generate(0.0f, sigma), 
+                                             NormalizedRandom.Generate(0.0f, sigma));
+
+        }
+        Debug.Log(coords_list.Count.ToString() + " particle generated.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody particle = Instantiate(projectile, transform.position, transform.rotation);
+            particle.velocity  = new Vector3(NormalizedRandom.Generate(0.0f, sigma),
+                                             NormalizedRandom.Generate(0.0f, sigma), 
+                                             NormalizedRandom.Generate(0.0f, sigma));
+        }
+    }
+}
---
+++ Assets/Particle.cs
@@ -0,0 +1,74 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float sphere_radius;
+    public float epsilon;
+    public float temperature;
+    public float mass;
+
+    private float sigma; // sigma for gaussian in Maxwell distribution
+    private float kb;
+    private Rigidbody      m_Rigidbody;
+    private SphereCollider m_SphereCollider;
+    private float box_size;
+    private float force_range;
+    private float dump_coef;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody       = GetComponent<Rigidbody>();
+        m_SphereCollider  = GetComponent<SphereCollider>();
+        sphere_radius              = 0.5f;
+        m_SphereCollider.radius    = 2.5f * sphere_radius;
+        m_SphereCollider.isTrigger = true;
+        box_size    = 5.0f;
+        //dump_coef   = 1.0f;
+        epsilon     = 0.05f;
+        m_Rigidbody.useGravity = false;
+        mass                   = 1.0f;
+        m_Rigidbody.mass       = mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        //Vector3 force_vec -= dump_coef * m_Rigidbody.velocity.magnitude * m_Rigidbody.velocity;
+        //m_Rigidbody.AddForce(force_vec); 
+
+        // fix by Periodic Boundary Condition
+        Vector3 currentPos = transform.position;
+        Vector3 currentVel = m_Rigidbody.velocity;
+        if (Mathf.Abs(currentPos.x) > box_size)
+        {
+            currentVel.x = -currentVel.x;
+        }
+        if(Mathf.Abs(currentPos.y) > box_size)
+        {
+            currentVel.y = -currentVel.y;
+        }
+        if (Mathf.Abs(currentPos.z) > box_size)
+        {
+            currentVel.z = -currentVel.z;
+        }
+        m_Rigidbody.velocity = currentVel;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Vector3 dist_vec = other.attachedRigidbody.position - transform.position;
+        float rinv       = 1.0f / dist_vec.magnitude;
+        float r1s1       = sphere_radius * rinv;
+        float r3s3       = r1s1 * r1s1* r1s1;
+        float r6s6       = r3s3 * r3s3;
+        float r12s12     = r6s6 * r6s6;
+        float derivative = 24.0f * epsilon * (r6s6 - 2.0f * r12s12) * rinv;
+        m_Rigidbody.AddForce(derivative * rinv * dist_vec);
+    }
+}
