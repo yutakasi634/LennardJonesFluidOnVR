d609d591ca318950e51d7b432f8415e3f48349cb
Yutaka Murata <p7dea87c7e4n7j@gmail.com>
2020-11-18 07:52:54 +0900

    refactor: comment for NormalizedRandom

--- Assets/NormalizedRandom.cs
+++ Assets/NormalizedRandom.cs
@@ -1,6 +1,7 @@
 using System.Security.Cryptography;
 using UnityEngine;
 
+// Random number generator for Normalized distribution
 public class NormalizedRandom
 {
     private bool  has_stock;
@@ -10,12 +11,13 @@ public class NormalizedRandom
     {
         has_stock = false;
     }
+
+    // Genarate random number from normalized distribution
+    // which specified by mean and standard deviation.
     public float Generate(float mean, float standard_deviation)
     {
         if (!has_stock)
         {
-            // generate normalized random number by box muller method.
-            // Normalized distribution is mean = 0.0. variance = 1.0.
             float x = Random.Range(0.0f, 1.0f);
             float y = Random.Range(0.0f, 1.0f);
             float random = Mathf.Sqrt(-2.0f * Mathf.Log(x)) * Mathf.Cos(2.0f * Mathf.PI * y);
